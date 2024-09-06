a40e68a8f1bf2004b6f8e86df1d9de1e7ae5b52d
Vincent Acanfora <brodino96@gmail.com>
2024-09-06 15:36:58 +0200

    Added open panel function (its still empty)

--- client/menu.lua
+++ client/menu.lua
@@ -1,5 +1,10 @@
 ---------------------- # ---------------------- # ---------------------- # ----------------------
--- Events
+
+local function openPanel()
+    -- Custom UI or ox_lib
+end
+
+---------------------- # ---------------------- # ---------------------- # ----------------------
 
 RegisterNetEvent("br_races:openPanel")
 AddEventHandler("br_races:openPanel", openPanel)
