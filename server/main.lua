0765d9b17ffba57723101c520a5672b646570958
Vincent Acanfora <brodino96@gmail.com>
2024-09-06 11:06:21 +0200

    Created function to check if the player can access, registred command to open menù

---
+++ server/main.lua
@@ -0,0 +1,35 @@
+---------------------- # ---------------------- # ---------------------- # ----------------------
+-- Functions
+
+local function getJobs(playerId)
+    if Config.jobSystem == "esx" then
+        return { [1] = ESX.GetPlayerFromId(playerId).getJob().name }
+    elseif Config.jobSystem == "br_multiJob" then
+        return exports["br_multiJob"]:getJobs()
+    end
+end
+
+function CanAccess(playerId)
+    local jobs = getJobs(playerId)
+    for i = 1, #jobs do
+        for k = 1, #Config.jobs do
+            if jobs[i] == Config.jobs[k] then
+                return true
+            end
+        end
+    end
+    return false
+end
+
+---------------------- # ---------------------- # ---------------------- # ----------------------
+-- Commands
+
+RegisterCommand("racemanager", function (source, args)
+    if not CanAccess(source) then
+        return TriggerClientEvent("ox_lib:notify", source, { type = "error", title = L("command:cannot_access")})
+    end
+
+    TriggerClientEvent("br_races:openPanel", source)
+end, false)
+
+---------------------- # ---------------------- # ---------------------- # ----------------------
