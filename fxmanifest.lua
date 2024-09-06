8384e8d4d824d721b2c03b09943851e7b2e48d2f
Vincent Acanfora <brodino96@gmail.com>
2024-09-06 09:56:25 +0200

    Created and initial config

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+author "Brodino"
+version "dev"
+description "A racing script that I would actually use"
+
+shared_scripts { "@oxmysql/lib/MySQL.lua", "config.lua", }
+client_scripts { "client/*", }
+server_scripts { "server/*", }
+
+dependencies { "oxmysql", }
