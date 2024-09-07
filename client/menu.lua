272cb133f26428c13f75101a59ab90b099c070c5
Vincent Acanfora <brodino96@gmail.com>
2024-09-07 17:33:22 +0200

    Registred callback for editing tracks

--- client/menu.lua
+++ client/menu.lua
@@ -9,6 +9,33 @@ end
 
 ---------------------- # ---------------------- # ---------------------- # ----------------------
 
+-- Generating a new track
+RegisterNUICallback("startRecording", function (body, cb)
+    StartTrackRecording()
+end)
+
+RegisterNUICallback("pauseRecording", function (body, cb)
+    PauseTrackRecording()
+end)
+
+RegisterNUICallback("startEditMode", function (body, cb)
+    EditTrack()
+end)
+
+RegisterNUICallback("stopEditoMode", function (body, cb)
+    StopEditMode()
+end)
+
+RegisterNUICallback("saveTrack", function (data, cb)
+    TriggerServerEvent("br_racing:addNewRace", {
+        racename = data.racename, owner = data.owner, public = data.public, track = json.encode(SaveTrack())
+    })
+end)
+
+-- TODO: Load races
+
+---------------------- # ---------------------- # ---------------------- # ----------------------
+
 RegisterNetEvent("br_races:openPanel")
 AddEventHandler("br_races:openPanel", openPanel)
 
