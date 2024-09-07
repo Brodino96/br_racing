994a9cc3ee5a526b6ab08bcc7159df7a76abcb39
Vincent Acanfora <brodino96@gmail.com>
2024-09-07 14:22:38 +0200

    Moved some functions to main.lua

--- client/menu.lua
+++ client/menu.lua
@@ -1,24 +1,15 @@
 ---------------------- # ---------------------- # ---------------------- # ----------------------
 
-local racesList = nil
-
----------------------- # ---------------------- # ---------------------- # ----------------------
-
 local function openPanel()
-    if not racesList then
-        racesList = lib.callback.await("br_racing:getRaces", false)
+    if not RacesList then
+        RacesList = lib.callback.await("br_racing:getRaces", false)
     end
-    SendNUIMessage({ action = "openPanel", info = racesList })
+    SendNUIMessage({ action = "openPanel", info = RacesList })
 end
 
 ---------------------- # ---------------------- # ---------------------- # ----------------------
 
 RegisterNetEvent("br_races:openPanel")
 AddEventHandler("br_races:openPanel", openPanel)
-
-RegisterNetEvent("br_racing:updateRaceList")
-AddEventHandler("br_racing:updateRaceList", function (newList)
-    racesList = newList
-end)
 
 ---------------------- # ---------------------- # ---------------------- # ----------------------
