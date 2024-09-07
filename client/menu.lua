0035b43480bcb59955008434c6f8388cc651dc1c
Vincent Acanfora <brodino96@gmail.com>
2024-09-07 11:38:08 +0200

    Added "openPanel" function

--- client/menu.lua
+++ client/menu.lua
@@ -1,12 +1,24 @@
 ---------------------- # ---------------------- # ---------------------- # ----------------------
 
+local racesList = nil
+
+---------------------- # ---------------------- # ---------------------- # ----------------------
+
 local function openPanel()
-    -- Custom UI or ox_lib
+    if not racesList then
+        racesList = lib.callback.await("br_racing:getRaces", false)
+    end
+    SendNUIMessage({ action = "openPanel", info = racesList })
 end
 
 ---------------------- # ---------------------- # ---------------------- # ----------------------
 
 RegisterNetEvent("br_races:openPanel")
 AddEventHandler("br_races:openPanel", openPanel)
+
+RegisterNetEvent("br_racing:updateRaceList")
+AddEventHandler("br_racing:updateRaceList", function (newList)
+    racesList = newList
+end)
 
 ---------------------- # ---------------------- # ---------------------- # ----------------------
