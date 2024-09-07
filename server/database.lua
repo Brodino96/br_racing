662bff6d6d092793f4025d9eae2d799863642c27
Vincent Acanfora <brodino96@gmail.com>
2024-09-07 08:53:39 +0200

    Completed database connection procedure

--- server/database.lua
+++ server/database.lua
@@ -9,11 +9,17 @@ AddEventHandler("onResourceStart", function (rName)
     local response = MySQL.transaction.await([[
         CREATE TABLE IF NOT EXISTS `br_racing` (
         `raceid` INT NOT NULL AUTO_INCREMENT,
-        `owner` VARCHAR(40),
-        `sharable` TINYINT(1),
+        `racename` VARCHAR(50),
+        `owner` VARCHAR(50),
+        `public` TINYINT(1),
         `track` LONGTEXT NOT NULL,
         PRIMARY KEY (`raceid`))
     ]])
+
+    if response then
+        return print("^0[^2INFO^0] Database connection established")
+    end
+    return print("^0[^2ERROR^0] Database connection failed")
 end)
 
 ---------------------- # ---------------------- # ---------------------- # ----------------------
