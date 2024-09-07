249973ac0edf3d1c4490e03743e8ed6229d41304
Vincent Acanfora <brodino96@gmail.com>
2024-09-07 08:53:30 +0200

    Added debug function

--- config.lua
+++ config.lua
@@ -2,4 +2,7 @@ Config = {
     locale = "en",
     jobSystem = "esx", -- "esx" or "br_multiJob"
     jobs = { "unemployed", "", "racer" }, -- list of jobs that have access to the script
+
+
+    debug = true,
 }
--- shared/utils.lua
+++ shared/utils.lua
@@ -3,3 +3,21 @@ Locale = {}
 function L(hash)
     return Locale[Config.locale][hash]
 end
+
+Debug = {
+    success = function (text)
+        if Config.debug then
+            print("[^2SUCCESS^0] "..text)
+        end
+    end,
+    info = function (text)
+        if Config.debug then
+            print("[^3INFO^0] "..text)
+        end
+    end,
+    error = function (text)
+        if Config.debug then
+            print("[^1ERROR^0] "..text)
+        end
+    end
+}
