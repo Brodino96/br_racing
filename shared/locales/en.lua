2631edf839fd9034001978270a2b5a8315691628
Vincent Acanfora <brodino96@gmail.com>
2024-09-08 12:45:35 +0200

    Added some locales

--- shared/locales/en.lua
+++ shared/locales/en.lua
@@ -1,3 +1,10 @@
 Locale["en"] = {
-    ["command:cannot_access"] = "You don't have enough permission to access this pannel"
+    ["confirm"] = "Confirm",
+    ["cancel"] = "Cancel",
+    ["command:cannot_access"] = "You don't have enough permission to access this pannel",
+    ["prompt:registration"] = "Press INPUT_CONTEXT to join the race",
+    ["dialog:registration_header"] = "Partecipate to the race",
+    ["dialog:registration_content"] = "Do you want to partecipate to the race for: ",
+    ["notify:not_enough_money"] = "You don't have enough "..exports.ox_inventory:Items(Config.currency).label.." to join this race!",
+    ["notify:joined:race"] = "You joined the race"
 }
